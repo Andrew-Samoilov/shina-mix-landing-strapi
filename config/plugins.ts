1c5efbf0c63f2fac5882aefc4d70b89f28fecdae
Andrew Samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2025-01-17 18:20:57 +0200

    add: data

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,6 +1,23 @@
-module.exports = ({ env }) => ({
+export default ({ env }) => ({
     i18n: {
-        enabled: true,
+      enabled: true,
     },
-});
-
+    // email: {
+    //   config: {
+    //     provider: 'nodemailer',
+    //     providerOptions: {
+    //       host: env('SMTP_HOST', 'smtp.example.com'), // зміни на свою поштову службу
+    //       port: env('SMTP_PORT', 587),
+    //       auth: {
+    //         user: env('SMTP_USER'),
+    //         pass: env('SMTP_PASS'),
+    //       },
+    //     },
+    //     settings: {
+    //       defaultFrom: 'no-reply@yourdomain.com',
+    //       defaultReplyTo: 'no-reply@yourdomain.com',
+    //     },
+    //   },
+    // },
+  });
+  
