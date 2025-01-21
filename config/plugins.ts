220d1132653eb6b3098a25823ad73406b2e6786f
Andrew Samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2025-01-21 17:37:25 +0200

    feat: email plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,23 +1,22 @@
 export default ({ env }) => ({
-    i18n: {
-      enabled: true,
+  i18n: {
+    enabled: true,
+  },
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST', 'm.itim.com.ua'), // зміни на свою поштову службу
+        port: env('SMTP_PORT', 587),
+        auth: {
+          user: env('SMTP_USER'),
+          pass: env('SMTP_PASS'),
+        },
+      },
+      settings: {
+        defaultFrom: 'webmaster@shinamix.com',
+        defaultReplyTo: 'webmaster@shinamix.com',
+      },
     },
-    // email: {
-    //   config: {
-    //     provider: 'nodemailer',
-    //     providerOptions: {
-    //       host: env('SMTP_HOST', 'smtp.example.com'), // зміни на свою поштову службу
-    //       port: env('SMTP_PORT', 587),
-    //       auth: {
-    //         user: env('SMTP_USER'),
-    //         pass: env('SMTP_PASS'),
-    //       },
-    //     },
-    //     settings: {
-    //       defaultFrom: 'no-reply@yourdomain.com',
-    //       defaultReplyTo: 'no-reply@yourdomain.com',
-    //     },
-    //   },
-    // },
-  });
-  
+  },
+});
